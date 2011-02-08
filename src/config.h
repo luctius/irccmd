e0d3c4d7f84b9de7151ea14ee8ff79333a035106
corpeters <corpeters@incas3.eu>
2011-02-08 12:02:40 +0100

    fixed a minor error in configure script

--- src/config.h
+++ src/config.h
@@ -2,9 +2,9 @@
 #define config_h_
 
 #include <stdbool.h>
-#include <lua5.1/lua.h>
-#include <lua5.1/lualib.h>
-#include <lua5.1/lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
 
 #include "main.h"
 
